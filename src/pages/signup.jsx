faaef9fb069424a3bd325c5d8593c1cc020483e0
Preet Shah <shahpreetk@gmail.com>
2020-10-08 17:35:04 +0530

    react setup and cleanup

---
+++ src/pages/signup.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class signup extends Component {
+    render() {
+        return (
+            <div>
+                Signup page
+            </div>
+        )
+    }
+}
+
+export default signup
