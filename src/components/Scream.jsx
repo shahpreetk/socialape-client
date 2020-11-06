bf1bc6159dcf7c522f46a86756be953ff7929586
Preet Shah <shahpreetk@gmail.com>
2020-11-06 20:15:28 +0530

    homepage done

--- src/components/Scream.jsx
+++ src/components/Scream.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Link from 'react-router-dom/Link'
+import {Link} from 'react-router-dom'
 import withStyles from '@material-ui/core/styles/withStyles'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
