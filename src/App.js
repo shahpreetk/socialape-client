7ce193bdb00f99db700d8c3106a69f0fda2f6adb
Preet Shah <shahpreetk@gmail.com>
2020-11-10 22:17:05 +0530

    redirect to / on loggedin

--- src/App.js
+++ src/App.js
@@ -8,26 +8,23 @@ import Navbar from './components/Navbar';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
+import AuthRoute from './util/AuthRoute'
+import themeFile from './util/theme'
+import jwtDecode from 'jwt-decode'
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#33c9dc',
-      main: '#00bcd4',
-      dark: '#008394',
-      contrastText: '#fff'
-    },
-    secondary: {
-      light: '#ff6333',
-      main: '#ff3d00',
-      dark: '#b22a00',
-      contrastText: '#fff'
-    }
-  },
-  typography: {
-    useNextVariants: true
+const theme = createMuiTheme(themeFile)
+
+let authenticated;
+const token = localStorage.FBIdToken;
+if (token){
+  const decodedToken = jwtDecode(token)
+  if(decodedToken.exp * 1000 < Date.now()){
+    window.location.href = '/login'
+    authenticated = false
+  } else {
+    authenticated = true
   }
-})
+}
 
 class App extends Component {
   render() {
@@ -39,8 +36,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path='/' component={home} />
-                <Route exact path='/login' component={login} />
-                <Route exact path='/signup' component={signup} />
+                <AuthRoute exact path='/login' component={login} authenticated={authenticated}/>
+                <AuthRoute exact path='/signup' component={signup} authenticated={authenticated}/>
               </Switch>
             </div>
           </Router>
