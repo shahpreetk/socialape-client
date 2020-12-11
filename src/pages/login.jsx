553fcbd10846e2afe16d816957b2e71c624b9b2a
Preet Shah <shahpreetk@gmail.com>
2020-12-11 18:25:03 +0530

    userReducer and login connections with redux

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from 'prop-types'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import AppIcon from '../images/icon.png'
 import Grid from '@material-ui/core/Grid'
@@ -9,6 +8,9 @@ import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+//  redux stuff
+import { connect } from 'react-redux';
+import { loginUser } from '../redux/actions/userActions'
 
 const styles = (theme) => ({
     ...theme.loginTheme
@@ -28,8 +30,8 @@ class login extends Component {
         const userData = {
             email: this.state.email,
             password: this.state.password
-        }
-
+        };
+        this.props.loginUser(userData, this.props.history);
     }
     handleChange = (event) => {
         this.setState({
@@ -67,7 +69,19 @@ class login extends Component {
 }
 
 login.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    loginUser: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(login)
+const mapStateToProps = state => ({
+    user: state.user,
+    UI: state.UI
+})
+
+const mapActionsToProps = {
+    loginUser
+}
+
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(login))
