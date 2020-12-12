b0545438d7b616d895309d179b3f4c2c59af8ce3
Preet Shah <shahpreetk@gmail.com>
2020-12-12 16:33:44 +0530

    app.js and authroutes connected with redux

--- src/App.js
+++ src/App.js
@@ -12,18 +12,22 @@ import themeFile from './util/theme'
 import jwtDecode from 'jwt-decode'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import {SET_AUTHENTICATED} from './redux/types'
+import {logoutUser, getUserData} from './redux/actions/userActions'
+import axios from 'axios'
 
 const theme = createMuiTheme(themeFile)
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token)
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser())
     window.location.href = '/login'
-    authenticated = false
   } else {
-    authenticated = true
+    store.dispatch({type:SET_AUTHENTICATED})
+    axios.defaults.headers.common['Auhorization'] = token
+    store.dispatch(getUserData())
   }
 }
 
@@ -37,8 +41,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path='/' component={home} />
-                <AuthRoute exact path='/login' component={login} authenticated={authenticated} />
-                <AuthRoute exact path='/signup' component={signup} authenticated={authenticated} />
+                <AuthRoute exact path='/login' component={login} />
+                <AuthRoute exact path='/signup' component={signup} />
               </Switch>
             </div>
           </Router>
