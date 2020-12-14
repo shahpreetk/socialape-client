3fdd7dee47eb9925fb8513f14ae61cfcc400e284
Preet Shah <shahpreetk@gmail.com>
2020-12-14 18:50:02 +0530

    image upload

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,15 +5,19 @@ import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 
 import { connect } from 'react-redux'
+import {logoutUser, uploadImage} from '../redux/actions/userActions'
 
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import MuiLink from '@material-ui/core/Link'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
 
 import LocationOn from '@material-ui/icons/LocationOn'
 import LinkIcon from '@material-ui/icons/Link'
 import CalendarToday from '@material-ui/icons/CalendarToday'
+import EditIcon from '@material-ui/icons/Edit'
 
 const styles = (theme) => ({
     paper: {
@@ -64,6 +68,16 @@ const styles = (theme) => ({
 })
 
 class Profile extends Component {
+    handleImageChange = (event) => {
+        const image = event.target.files[0]
+        const formData = new FormData()
+        formData.append('image', image, image.name)
+        this.props.uploadImage(formData)
+    }
+    handleEditPicture = () => {
+        const fileInput = document.getElementById('imageInput')
+        fileInput.click()
+    }
     render() {
         const { classes, user: { credentials: { userHandle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = this.props
 
@@ -71,7 +85,13 @@ class Profile extends Component {
             <Paper className={classes.paper}>
                 <div className={classes.profile}>
                     <div className="image-wrapper">
-                        <img src={imageUrl} alt="profile" className="profile-image"/>
+                        <img src={imageUrl} alt="profile" className="profile-image" />
+                        <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
+                        <Tooltip title="Edit Profile Picture" placement="top">
+                            <IconButton onClick={this.handleEditPicture} className="button">
+                                <EditIcon color="primary" />
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <hr />
                     <div className="profile-details">
@@ -96,27 +116,27 @@ class Profile extends Component {
                                 <a href={website} target="_blank" rel="noopener noreferrer">
                                     {" "}{website}
                                 </a>
-                                <hr/>
+                                <hr />
                             </>
                         )}
-                        <CalendarToday color="primary"/>{" "}
+                        <CalendarToday color="primary" />{" "}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
                 </div>
             </Paper>
         ) : (
-            <Paper className={classes.paper}>
-                <Typography variant="body2" align="center">No profile found, please login</Typography>
-                <div className={classes.buttons}>
-                    <Button variant="contained" color="primary" component={Link} to="/login">
-                        Login
+                <Paper className={classes.paper}>
+                    <Typography variant="body2" align="center">No profile found, please login</Typography>
+                    <div className={classes.buttons}>
+                        <Button variant="contained" color="primary" component={Link} to="/login">
+                            Login
                     </Button>
-                    <Button variant="contained" color="secondary" component={Link} to="/signup">
-                        Signup
+                        <Button variant="contained" color="secondary" component={Link} to="/signup">
+                            Signup
                     </Button>
-                </div>
-            </Paper>
-        )) : (<p>Loading.....</p>)
+                    </div>
+                </Paper>
+            )) : (<p>Loading.....</p>)
 
         return profileMarkup
     }
@@ -126,9 +146,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 })
 
+const mapActionsToProps = {logoutUser, uploadImage}
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
