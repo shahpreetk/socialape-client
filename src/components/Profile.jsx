250530da07e3395591d54d44494cb74f37d036e1
Preet Shah <shahpreetk@gmail.com>
2020-12-15 00:53:26 +0530

    logout

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
 
 import { connect } from 'react-redux'
-import {logoutUser, uploadImage} from '../redux/actions/userActions'
+import { logoutUser, uploadImage } from '../redux/actions/userActions'
 
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
@@ -18,6 +18,7 @@ import LocationOn from '@material-ui/icons/LocationOn'
 import LinkIcon from '@material-ui/icons/Link'
 import CalendarToday from '@material-ui/icons/CalendarToday'
 import EditIcon from '@material-ui/icons/Edit'
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn'
 
 const styles = (theme) => ({
     paper: {
@@ -78,6 +79,9 @@ class Profile extends Component {
         const fileInput = document.getElementById('imageInput')
         fileInput.click()
     }
+    handleLogout = () => {
+        this.props.logoutUser()
+    }
     render() {
         const { classes, user: { credentials: { userHandle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = this.props
 
@@ -122,6 +126,11 @@ class Profile extends Component {
                         <CalendarToday color="primary" />{" "}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+                    <Tooltip title="Logout" placement="top">
+                        <IconButton onClick={this.handleLogout}>
+                            <KeyboardReturn color="primary" />
+                        </IconButton>
+                    </Tooltip>
                 </div>
             </Paper>
         ) : (
@@ -146,7 +155,7 @@ const mapStateToProps = (state) => ({
     user: state.user
 })
 
-const mapActionsToProps = {logoutUser, uploadImage}
+const mapActionsToProps = { logoutUser, uploadImage }
 
 Profile.propTypes = {
     logoutUser: PropTypes.func.isRequired,
