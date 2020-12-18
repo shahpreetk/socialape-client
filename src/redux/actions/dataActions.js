2812357cd5c58f7fff286ef7ea0acc4fcb6c06b7
Preet Shah <preet25@somaiya.edu>
2020-12-18 08:45:28 +0000

    comments can be added

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -10,6 +10,7 @@ import {
   LOADING_UI,
   SET_SCREAM,
   STOP_LOADING_UI,
+  SUBMIT_COMMENT,
 } from "../types";
 import axios from "axios";
 
@@ -57,7 +58,7 @@ export const postScream = (newScream) => (dispatch) => {
         type: POST_SCREAM,
         payload: res.data,
       });
-      dispatch({ type: CLEAR_ERRORS });
+      dispatch(clearErrors());
     })
     .catch((err) => {
       dispatch({
@@ -91,6 +92,25 @@ export const unlikeScream = (screamId) => (dispatch) => {
       });
     })
     .catch((err) => console.log(err));
+};
+
+// Submit Comment
+export const submitComment = (screamId, commentData) => (dispatch) => {
+  axios
+    .post(`/scream/${screamId}/comment`, commentData)
+    .then((res) => {
+      dispatch({
+        type: SUBMIT_COMMENT,
+        payload: res.data,
+      });
+      dispatch(clearErrors());
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
 };
 
 export const deleteScream = (screamId) => (dispatch) => {
