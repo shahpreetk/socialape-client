2a8313c9688041132ee0569dc235ccf2266330c3
Preet Shah <preet25@somaiya.edu>
2020-12-19 18:44:13 +0000

    static profile made and user page completed

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Scream from "../components/scream/Scream";
+import StaticProfile from "../components/profile/StaticProfile";
 import Grid from "@material-ui/core/Grid";
 import { connect } from "react-redux";
 import { getUserData } from "../redux/actions/dataActions";
@@ -13,11 +14,14 @@ class user extends Component {
   componentDidMount() {
     const handle = this.props.match.params.userHandle;
     this.props.getUserData(handle);
-    axios.get(`/user/${handle}`).then((res) => {
-      this.setState({
-        profile: res.data.user,
-      }).catch((err) => console.log(err));
-    });
+    axios
+      .get(`/user/${handle}`)
+      .then((res) => {
+        this.setState({
+          profile: res.data.user,
+        });
+      })
+      .catch((err) => console.log(err));
   }
   render() {
     const { screams, loading } = this.props.data;
@@ -36,7 +40,11 @@ class user extends Component {
             {screamsMarkup}
           </Grid>
           <Grid item sm={4} xs={12}>
-            <StaticProfile profile={this.state.profile} />
+            {this.state.profile === null ? (
+              <p>Loading Profile</p>
+            ) : (
+              <StaticProfile profile={this.state.profile} />
+            )}
           </Grid>
         </Grid>
       </>
